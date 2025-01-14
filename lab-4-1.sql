8bb8d900f3ec1a089dbb7b34522a87ec05b13e91
Ross Blackwell <ross.blackwell@kellogg.northwestern.edu>
2025-01-14 22:29:59 +0000

    Completed through 4-1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,9 @@
 -- Expected result:
 -- 2935
 
+.mode columns
+.headers on
 
+SELECT players.first_name, players.last_name, SUM(stats.hits)
+FROM stats INNER JOIN players ON players.id = stats.player_id
+WHERE players.last_name = "Bonds" AND players.first_name = "Barry";
